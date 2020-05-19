a000bfb1a0ed9273714a1b40f61f2aea5ebd35fd
Giovanni Sabino <giosab@intra.olos.com.br>
2020-05-19 11:24:10 -0300

    add types for typescript projects

---
+++ index.d.ts
@@ -0,0 +1,20 @@
+declare module "@this-empathy/javascript-mask" {
+    export function account (value: string): string;
+    export function agency (value: string): string;
+    export function carPlate (value: string): string;
+    export function cep (value: string): string;
+    export function cnpj (value: string): string;
+    export function cpf (value: string): string;
+    export function cpfCnpj (value: string): string;
+    export function date (value: string): string;
+    export function hour (value: string): string;
+    export function letters (value: string): string;
+    export function millisecondsToDate (value: string): string;
+    export function millisecondsToDateHour (value: string): string;
+    export function millisecondsToHour (value: string): string;
+    export function money (value: string): string;
+    export function number (value: string): string;
+    export function phone (value: string): string;
+    export function rg (value: string): string;
+    export function percentage (value: string): string;
+}
