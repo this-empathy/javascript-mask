17d47b16ddd52f4f3e8ac0e6c1ef54af9d8b54e5
Giovanni Sabino <giosab@intra.olos.com.br>
2020-05-22 13:11:58 -0300

    add codes list

--- masks/date.js
+++ masks/date.js
@@ -1,6 +1,13 @@
+import Locales from '../utils/locales'
+
 export default(value) =>{
+
+    console.log(Locales.pt_BR.co)
+
     value = value.replace(/\D/g, '')
     value = value.replace(/(\d{2})(\d)/, '$1/$2')
     value = value.replace(/(\d{2})(\d)/, '$1/$2')
+
+    console.log(value)
     return value
 }
