9e148634452873fed294831e1612dda4e3e54646
Giovanni Sabino <giosab@intra.olos.com.br>
2020-06-01 11:56:39 -0300

    remove generate files and add geretae files from npm

--- masks/date.js
+++ masks/date.js
@@ -1,12 +1,11 @@
-import Locales from '../locales/locales.json'
 import { toPattern } from '../utils/utils'
+import Locales from '@this-empathy/locales-patterns'
 
 export default (value, locale = 'pt_BR') => {
 	locale = locale.replace('-', '_')
 	const format = Locales[locale] ? Locales[locale] : Locales.pt_BR
 
-	console.log(format);
-	
+	console.log(format)
 
 	const literal = /\W|_/g.exec(format.dateFormat)[0]
 	const opt = {
@@ -18,9 +17,5 @@ export default (value, locale = 'pt_BR') => {
 
 	// 9 is the code with represent just numbers on toPattern function
 	const pattern = format.dateFormat.replace(/[A-Z]/g, '9')
-
-	console.log(opt);
-	
-
 	return { value: toPattern(value, pattern), opt }
 }
