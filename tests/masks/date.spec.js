47f85228d611de12131a3093001a3be3a6873dcb
Giovanni Sabino <giosab@intra.olos.com.br>
2020-06-02 16:15:51 -0300

    start bug fix

--- tests/masks/date.spec.js
+++ tests/masks/date.spec.js
@@ -2,10 +2,11 @@ import mask from '../../index.js'
 
 describe('Date', () => {
   test('Mascara para data', () => {
-    mask.date('08011991')
-    mask.date('08011991', 'es-CL')
-    mask.date('1081991', 'en_US')
-    mask.date('19910801', 'rw')
-    mask.date('08011991', 'null')
+    // mask.date('08011991')
+    // mask.date('08011991', 'es-CL')
+    mask.date('1081991', 'en_US') // == 1/08/1991
+    mask.date('11081991', 'en_US') // == 11/08/1991
+    // mask.date('19910801', 'rw')
+    // mask.date('08011991', 'null')
   });
 });
