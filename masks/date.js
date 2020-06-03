c4c7c4a7d2a24b0cf147699c20e37da9f320bb5f
Giovanni Sabino <giosab@intra.olos.com.br>
2020-06-03 10:07:11 -0300

    fix two digits bug

--- masks/date.js
+++ masks/date.js
@@ -5,9 +5,6 @@ export default (value, locale = 'pt_BR') => {
 	locale = locale.replace('-', '_')
 	const format = Locales[locale] ? Locales[locale] : Locales.pt_BR
 
-	console.log(format.dateFormat);
-	
-
 	const literal = /\W|_/g.exec(format.dateFormat)[0]
 	const opt = {
 		literal: literal,
@@ -18,9 +15,5 @@ export default (value, locale = 'pt_BR') => {
 
 	// 9 is the code with represent just numbers on toPattern function
 	const pattern = format.dateFormat.replace(/[A-Z]/g, '9')
-
-	console.log(toPattern(value, pattern));
-	
-
 	return { value: toPattern(value, pattern), opt }
 }
